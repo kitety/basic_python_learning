37ec271f0792f383341cbaa6828234c3be8de711
kitety <aizaizuori@foxmail.com>
2018-08-05 23:29:01 +0800

    列表的增删改查

---
+++ day1-day10/day4/02.py
@@ -0,0 +1,44 @@
+# li = ['alex', [1, 2, 3, 4, 5], 'beijing', '女神', 'taipei']
+# print(li[0], li[1], li[2])
+# print(li[:3])
+# 列表的增删改查
+li = ['alex', [1, 2, 3, 4, 5], 'beijing', '女神', 'taipei']
+# li.append(45)
+# print(li)
+# 一个小例子
+# li = []
+# inpBool = True
+# while inpBool:
+#     inpVal = input('请输入一个值,输入q退出:')
+#     if inpVal.lower().strip() == 'q':
+#         inpBool = False
+#     else:
+#         li.append(inpVal)
+# print('结束,当前列表为', li)
+
+# 增
+li = ['alex', [1, 2, 3, 4, 5], 'beijing', '女神', 'taipei']
+li.insert(4, 'test')
+li.extend('二哥')
+li.extend([1, 2, 3])
+# print(li)
+
+# 删除
+li = ['alex', [1, 2, 3, 4, 5], 'beijing', '女神', 'taipei']
+# print(li.pop(), li)
+# li.remove('alex')
+# print(li)
+# li.clear()
+# print(li)
+# del li
+# print(li)
+# 顾头不顾尾
+# del li[2:]
+# del li[:2]
+# print(li)
+
+# 改动
+li = ['alex', [1, 2, 3, 4, 5], 'beijing', '女神', 'taipei']
+li[0] = [1, 2, 3]
+li[0:2] = [1, 2]
+print(li)
