42a8f57092b3868597a7a87a3d70117b2083ca8c
kitety <aizaizuori@foxmail.com>
2018-08-16 00:17:03 +0800

    元组的开始

---
+++ day1-day10/day4/03.py
@@ -0,0 +1,12 @@
+# 列表的嵌套
+li = ['taibai', 'movie', '刘德华', ['alex', 123], 98]
+# print('刘德华的德：', li[2][1])
+# 首字母大写
+# name = li[0].capitalize()
+# print('taibai大写：', name)
+# li[0] = name
+# li[1] = li[1].replace('vie', 've')
+# li[1]='cool'
+# print(li)
+li[3][0]=li[3][0].upper()
+print(li)
---
+++ day1-day10/day4/tuple.py
@@ -0,0 +1,6 @@
+# 元组
+tu = (123, 'alex', [1, 23, 45], 56)
+tu[2][0] = 8989
+print(tu[2][0])
+for i in tu:
+    print(i)
