d0c73d8d15a4b593ab4c3e8fd80f20aa08dc4d4d
kitety <aizaizuori@foxmail.com>
2018-09-09 01:27:19 +0800

    字典的删除元素的提升

--- day1-day10/day7/basic_all.py
+++ day1-day10/day7/basic_all.py
@@ -4,9 +4,34 @@ print(s.isspace())
 '''
 list:
 '''
+# 把数组的全部清空 用for循环
+# 第一种 从后面往前面删除  第二种 照出来新建
 li = [11, 22, 33, 44, 55]
 for i in range(len(li) - 1, -1, -1):
     print(i, li)
-    # print(li[li.index(i)])
-    del li[i]
+    # 加上判断的话就删除奇数
+    if i % 2 == 1:
+        del li[i]
 print(li)
+
+# 删除带有'k'的键值对
+dic = {'k1': 12, 'k2': 13, 'a1': 1}
+dic1 = {}
+# 换种思路
+for i in dic:
+    if 'k' not in i:
+        dic1[i] = dic[i]
+        # del dic[i]
+        # 报错,循环的时候不能删除
+dic = dic1
+print(dic)
+
+# 另外的思路 获取key 放入数组再循环删除
+dic = {'k1': 12, 'k2': 13, 'a1': 1}
+li = []
+for i in dic:
+    if 'k' in i:
+        li.append(i)
+for i in li:
+    del dic[i]
+print(dic)
