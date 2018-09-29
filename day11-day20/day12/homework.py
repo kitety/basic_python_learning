0ca161039d7e063938229259463105cdc12624f6
kitety <aizaizuori@foxmail.com>
2018-09-29 23:06:15 +0800

    homework-12

--- day11-day20/day12/homework.py
+++ day11-day20/day12/homework.py
@@ -1,3 +1,6 @@
+import os
+from urllib.request import urlopen
+
 # 1.编写装饰器，为多个函数加上认证的功能（用户的账号密码来源于文件），
 # 要求登录成功一次，后续的函数都无需再输入用户名和密码
 FLAG = False
@@ -58,8 +61,32 @@ def shoplist_del1():
     print('删除商品')
 
 
-shoplist_add1()
+# shoplist_add1()
 
 # 1.编写下载网页内容的函数，要求功能是：用户传入一个url，函数返回下载页面的结果
 # 2.为题目1编写装饰器，实现缓存网页内容的功能：
 # 具体：实现下载的页面存放于文件中，如果文件内有值（文件大小不为0），就优先从文件中读取网页内容，否则，就去下载，然后存到文件中
+
+
+def cache(func):
+    def inner(*args, **kwargs):
+        if os.path.getsize('web_cache'):
+            with open('web_cache', 'rb') as f:
+                return f.read()
+        ret = func(*args, **kwargs)
+        with open('web_cache', 'wb') as f:
+            f.write(b'*********' + ret)
+        return ret
+
+    return inner
+
+
+@cache
+def get(url):
+    code = urlopen(url).read()
+    return code
+
+
+print(get('http://www.baidu.com'))
+print(get('http://www.baidu.com'))
+print(get('http://www.baidu.com'))
