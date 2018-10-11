51f4c2f288acd997b1b84a6df5a3824b6e6b22ce
kitety <aizaizuori@foxmail.com>
2018-10-11 22:37:07 +0800

    文件的监视

--- day11-day20/day13/listentofile.py
+++ day11-day20/day13/listentofile.py
@@ -1,6 +1,12 @@
-f = open('hhh', encoding="utf-8")
-# 不用for readline 都是一次性的
-while 1:
-    line = f.readline()
-    if line:
-        print(line)
+def tail(filename):
+    f = open(filename, encoding="utf-8")
+    # 不用for readline 都是一次性的
+    while 1:
+        line = f.readline()
+        if line.strip():
+            yield line.strip()
+
+
+g = tail('hhh')
+for i in g:
+    print(i)
