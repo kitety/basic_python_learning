ceb2d414daccc84380eff66aaa13be8fc5dcc589
kitety <aizaizuori@foxmail.com>
2019-01-14 00:13:43 +0800

    字节转换

--- day11-day20/day16/review.py
+++ day11-day20/day16/review.py
@@ -18,13 +18,30 @@ sli = slice(1, 5, 2)
 
 # format() 保留小数，浮点数，进制转换
 # 格式调整 左对齐，右对齐，居中
-print(format('test', '<20'))
-print(format('test', '>20'))
-print(format('test', '^20'))
+# print(format('test', '<20'))
+# print(format('test', '>20'))
+# print(format('test', '^20'))
 
 # bytes转换成bytes类型
 # bytes：unicode->utf8/gbk
-# gbk->utf8:decode->encode
-# 两个的编码不同
-print(bytes('你好', encoding='GBK').decode('GBK'))
-print(bytes('你好', encoding='utf-8'))
+# gbk->utf8:decode->Unicode->encode
+# 两个的编码不同，需要转换
+'''
+bytes('你好', encoding='GBK')：Unicode的“你好”转成GBK编码，
+bytes('你好', encoding='GBK').decode('GBK') 再decode成Unicode
+bytes('你好', encoding='GBK').decode('GBK').encode('utf-8') 再编码为utf-8
+'''
+# print(bytes('你好', encoding='GBK').decode('GBK'))
+# print(bytes('你好', encoding='GBK').decode('GBK').encode('utf-8'))
+# print(bytes('你好', encoding='utf-8'))
+
+# 网络编程 只能传二进制
+# 照片和视频 二进制
+# html网页爬虫也是bytes编码
+
+# bytearray byte类型的数组
+by_arr = bytearray('你好', encoding='utf-8')
+# 十六进制转换
+print(by_arr)
+# 长的字节可以指定位置替换
+print(by_arr[0])
