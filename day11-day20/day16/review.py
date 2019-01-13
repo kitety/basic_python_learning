df5a070bb9016e0f17b105aa287789559fc5646c
kitety <aizaizuori@foxmail.com>
2019-01-13 23:57:55 +0800

    更新

--- day11-day20/day16/review.py
+++ day11-day20/day16/review.py
@@ -18,6 +18,13 @@ sli = slice(1, 5, 2)
 
 # format() 保留小数，浮点数，进制转换
 # 格式调整 左对齐，右对齐，居中
-print(format('test', '<20')) 
+print(format('test', '<20'))
 print(format('test', '>20'))
 print(format('test', '^20'))
+
+# bytes转换成bytes类型
+# bytes：unicode->utf8/gbk
+# gbk->utf8:decode->encode
+# 两个的编码不同
+print(bytes('你好', encoding='GBK').decode('GBK'))
+print(bytes('你好', encoding='utf-8'))
---
+++ 参考网站/website.md
@@ -0,0 +1,3 @@
+参考博客
+===
+- Eva-J: https://www.cnblogs.com/Eva-J/p/7277026.html
